9e56ad97904cae34f402515498ac0e77ea4202c3
dLangton <dlangston@ezesoft.com>
2019-11-03 22:46:31 -0500

    added failsafe code and bottom out timming

--- FishControl_FINAL.ino/FishControl_FINAL.ino.ino
+++ FishControl_FINAL.ino/FishControl_FINAL.ino.ino
@@ -2,13 +2,19 @@
     Fish waits a random time between 20 and 30 minutes
     Fish sinks
     Fish waits for a random time between 20 and 30 minutes
-
     Working
+
+  Disaster switch is wired with ground and a digitalPin
+
+
 */
 
 // Enables
 const int dirPin = 11;  // direction motor
 const int pwmPin = 6;  // pwm pin for motor
+const int currentSensPin = A0;  // current sensor signal pin
+const int disasterPin = 2;
+
 
 unsigned long startSinkTime;
 unsigned long startWaitBottomTime;
@@ -19,19 +25,22 @@ unsigned long startWaitTopTime;
 int SINK_SPEED = 50;
 int RISE_SPEED = 50;
 // timing of states - you neeed to change these
-const unsigned long SINK_TIME = 35000;
-const unsigned long ASCEND_TIME = 40000;
+const unsigned long SINK_TIME = 55000;
+const unsigned long ASCEND_TIME = 35000;
 
 const unsigned long WAIT_BOTTOM_TIME_MIN = 5000;
 const unsigned long WAIT_BOTTOM_TIME_MAX = 10000;
 const unsigned long WAIT_TOP_TIME_MIN = 5000;
 const unsigned long WAIT_TOP_TIME_MAX = 10000;
 
-// motor directions
+const int lockedFishADC = 275;  // change this to empirical value
+
+// motor directions - change these if it goes the wrong way at startup
 const int SINK = 0;
 const int RISE = 1;
 
 int state;
+int currentADC; // current sensor reading
 
 #define SINKING 0
 #define WAIT_ON_BOTTOM 1
@@ -45,10 +54,15 @@ void setup() {
   pinMode(dirPin, OUTPUT);
   pinMode(pwmPin, OUTPUT);
   state = SINKING;
+  pinMode(disasterPin, INPUT_PULLUP);
   delay(2000);
 }
 
 void loop() {
+
+  readCurrentSensor();
+  checkDistasterSwitch();  // comment in to add disaster sensor
+
   switch (state)
   {
     case SINKING:
@@ -83,12 +97,21 @@ void sink_fish() {
     analogWrite(pwmPin, SINK_SPEED);
   }
 
+  if (currentADC > lockedFishADC) { // cable stop against the bottom weight
+    // cut off drive and go to next state
+    init = 0;
+    state = WAIT_ON_BOTTOM;
+    Serial.println("bail out - fish is locked on bottom");
+
+  }
+
   if (millis() - startRiseTime > ASCEND_TIME) {
     // time's up - all done
     init = 0;
     state = WAIT_ON_BOTTOM;
   }
 }
+
 //////////////////ASCEND///////////////////
 void raise_fish() {
   int static init = 0;
@@ -118,7 +141,6 @@ void wait_on_bottom() {
   int static init = 0;
   unsigned long static topTime;
 
-
   if (!init) {
     startWaitTopTime = millis(); // time that ascend start - make sure this happens only once
     init = 1;
@@ -136,15 +158,13 @@ void wait_on_bottom() {
   }
 }
 
-
-//////////////////WATE ON SURFACE////////////////
+//////////////////WAIT ON SURFACE////////////////
 void wait_on_surface() {
   int static init = 0;
   unsigned long static topTime;
 
-
   if (!init) {
-    startWaitTopTime = millis(); // time that ascend start - make sure this happens only once
+    startWaitTopTime = millis(); // time that ascend start
     init = 1;
     topTime = random (WAIT_TOP_TIME_MIN, WAIT_TOP_TIME_MAX);
     Serial.print("wait on surface\t");
@@ -158,4 +178,44 @@ void wait_on_surface() {
     init = 0;
     state = SINKING;
   }
+}
+
+void readCurrentSensor() {
+  static unsigned long lastPrintMillis;
+  int rawADC = analogRead(currentSensPin);
+  static float smoothedVal;
+  static float filterVal = 0.97;
+
+  smoothedVal = smooth((float)rawADC, filterVal, smoothedVal);
+  currentADC  = smoothedVal;
+
+  if (millis() - lastPrintMillis > 1000) { // controls Serial debugging of current value
+    Serial.print(rawADC);
+    Serial.print("\t");
+    Serial.println(smoothedVal);
+    lastPrintMillis = millis();
+  }
+}
+
+void checkDistasterSwitch() {
+  if (!digitalRead(disasterPin)) {  // wire for normally open switch
+    analogWrite(pwmPin, 0);
+    while (1) {
+      Serial.println("Disaster sensor is triggered!");
+      delay(1000);
+    }
+  }
+}
+
+
+float smooth(float data, float filterVal, float smoothedVal) {
+  if (filterVal > 1.0) {     // check to make sure param's are within range
+    filterVal = .999;
+  }
+  else if (filterVal <= 0) {
+    filterVal = 0;
+  }
+  smoothedVal = (data * (1 - filterVal)) + (smoothedVal  *  filterVal);
+
+  return smoothedVal;
 }
